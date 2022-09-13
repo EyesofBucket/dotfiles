09e7ff5d01a050f14e09855407f2ecef626b0ee1
EyesofBucket <48115604+EyesofBucket@users.noreply.github.com>
2022-09-13 14:56:36 -0500

    Added update.sh

---
+++ update.sh
@@ -0,0 +1,3 @@
+# sh -c "$(curl -fsSL https://raw.github.com/eyesofBucket/configs/main/update.sh)" ""
+wget https://raw.github.com/eyesofBucket/configs/main/dotfiles/.zshrc -o ~/.zshrc
+wget https://raw.github.com/eyesofBucket/configs/main/dotfiles/.vimrc -o ~/.vimrc
