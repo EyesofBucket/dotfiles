29046ac9998f9e446bdda83aa6e8a70680df9e73
EyesofBucket <48115604+EyesofBucket@users.noreply.github.com>
2022-09-13 13:52:50 -0500

    Moved dotfiles

--- setup.sh
+++ setup.sh
@@ -28,4 +28,4 @@ rm ~/.poshthemes/themes.zip
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Add config files
-cp ./.* $HOME/
+cp -r ./dotfiles/* $HOME/
