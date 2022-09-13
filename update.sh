aefa079785359c8abd06baab7c3ab3b4ddae75b6
EyesofBucket <48115604+EyesofBucket@users.noreply.github.com>
2022-09-13 15:35:10 -0500

    Reduced wget verbosity

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 # sh -c "$(curl -fsSL https://raw.github.com/eyesofBucket/configs/main/update.sh)" ""
-wget -nv https://raw.github.com/eyesofBucket/configs/main/dotfiles/.zshrc -O ~/.zshrc
-wget -nv https://raw.github.com/eyesofBucket/configs/main/dotfiles/.vimrc -O ~/.vimrc
+wget --no-verbose https://raw.github.com/eyesofBucket/configs/main/dotfiles/.zshrc -O ~/.zshrc
+wget --no-verbose https://raw.github.com/eyesofBucket/configs/main/dotfiles/.vimrc -O ~/.vimrc
