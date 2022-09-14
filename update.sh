8a82b10cb0a3451c5df3a7dfd95ceba48eb700df
EyesofBucket <48115604+EyesofBucket@users.noreply.github.com>
2022-09-14 09:21:53 -0500

    Added vim-plug install step

--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
 # sh -c "$(curl -fsSL https://raw.github.com/eyesofBucket/configs/main/update.sh)" ""
 wget --no-verbose https://raw.github.com/eyesofBucket/configs/main/dotfiles/.zshrc -O ~/.zshrc
 wget --no-verbose https://raw.github.com/eyesofBucket/configs/main/dotfiles/.vimrc -O ~/.vimrc
+vim -c "PlugInstall" -c "qa"
