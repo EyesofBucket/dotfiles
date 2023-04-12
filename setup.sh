2a398e8ff3605fa66e837071c791c5336b62bd3a
EyesofBucket <drufus6541@gmail.com>
2023-04-12 13:40:03 -0500

    Added sudoers configs

--- setup.sh
+++ setup.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
+#!/bin/bash
 
-# Specify branch
-if [ -z "$0" ]
-then
-    branch="main"
-else
-    branch="$0"
-fi
+branch="main"
+
+while getopts 'ab:h' opt; do
+  case "$opt" in
+    a)
+      all=true
+      ;;
+
+    b)
+      branch="$OPTARG"
+      ;;
+   
+    ?|h)
+      echo "Usage: setup.sh [-a] [-b branch]"
+      exit 1
+      ;;
+  esac
+done
+shift "$(($OPTIND -1))"
 
 # Install requirements
 PACKAGES='curl wget zsh git vim'
@@ -55,3 +68,8 @@ wget --no-verbose "https://raw.github.com/eyesofBucket/configs/$branch/dotfiles/
 # Install vim plugins as listed in the config file
 vim --not-a-term -c "PlugInstall" -c "%w /tmp/vim.log" -c "qa" >/dev/null
 cat /tmp/vim.log
+
+if [ "$all" = true ]
+then
+    wget --no-verbose "https://raw.github.com/eyesofBucket/configs/$branch/dotfiles/sudoers_eyesofbucket" -O /etc/sudoers.d/sudoers_eyesofbucket
+fi
