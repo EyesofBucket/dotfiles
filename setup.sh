1a75b0fe6e756a7a7717c9ad1c6c540f7bbb98aa
eyesofbucket <eyesofbucket@example.com>
2023-10-14 16:35:04 -0500

    Added root support

--- setup.sh
+++ setup.sh
@@ -6,6 +6,12 @@ EOF
 exit 1
 }
 
+sudo=""
+if [ "$EUID" -ne 0 ]
+  then
+  sudo="sudo "
+fi
+
 branch="main"
 
 # Arg validation
@@ -34,15 +40,15 @@ PACKAGES='curl wget zsh git vim'
 
 # Debian
 if which apt >/dev/null; then
-    sudo apt install -y $PACKAGES
+   $(sudo)apt install -y $PACKAGES
 
 # Fedora
 elif which yum >/dev/null; then
-    sudo yum install -y $PACKAGES
+    $(sudo)yum install -y $PACKAGES
 
 # Arch
 elif which pacman >/dev/null; then
-    sudo pacman -S --noconfirm $PACKAGES
+    $(sudo)pacman -S --noconfirm $PACKAGES
 else
     printf "\033[0;31mUnable to install requirements: No package manager found.\033[0m\n" 1>&2
     exit 1
@@ -59,8 +65,8 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 
 # Install oh-my-posh
 declare -A arch=( ['x86_64']='amd64' ['aarch64']='arm64' ['armv71']='arm' )
-sudo wget "https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-${arch[$(uname -m)]}" -O /usr/local/bin/oh-my-posh
-sudo chmod a+rx /usr/local/bin/oh-my-posh
+$(sudo)wget "https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-${arch[$(uname -m)]}" -O /usr/local/bin/oh-my-posh
+$(sudo)chmod a+rx /usr/local/bin/oh-my-posh
 
 # Install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -70,6 +76,6 @@ bash -c "$(curl -fsSL https://raw.github.com/eyesofBucket/configs/$branch/update
 
 # Add config changes that would affect all users
 if [ "$all" = true ]; then
-    sudo wget --no-verbose "https://raw.github.com/eyesofBucket/configs/$branch/dotfiles/sudoers_eyesofbucket" -O /etc/sudoers.d/eyesofbucket
-    sudo chmod 440 /etc/sudoers.d/eyesofbucket
+    $(sudo)wget --no-verbose "https://raw.github.com/eyesofBucket/configs/$branch/dotfiles/sudoers_eyesofbucket" -O /etc/sudoers.d/eyesofbucket
+    $(sudo)chmod 440 /etc/sudoers.d/eyesofbucket
 fi
