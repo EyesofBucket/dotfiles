4c0198a871092867da6f3562e793d3082bfab10e
eyesofbucket <eyesofbucket@example.com>
2024-01-12 13:13:04 -0600

    Added pwsh pushd aliases

--- pwsh/alias.ps1
+++ pwsh/alias.ps1
@@ -1,2 +1,25 @@
+$Env:bvktpath = Join-Path $Env:LOCALAPPDATA "bvkt"
+$Env:nvpath = Join-Path $Env:LOCALAPPDATA "nvim"
+
 New-Alias which get-command
 New-Alias wta get-wtactions
+
+function bvktedit {
+    Push-Location $Env:bvktpath
+    nvim .
+    Pop-Location
+}
+
+function bvktconf {
+    Push-Location $Env:bvktpath
+}
+
+function nvedit {
+    Push-Location $Env:nvpath
+    nvim .
+    Pop-Location
+}
+
+function nvconf {
+    Push-Location $Env:nvpath
+}
