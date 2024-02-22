7f341c6bda0542bea8dfe2d99704906fe483b7b4
eyesofbucket <eyesofbucket@example.com>
2024-02-22 23:02:51 +0000

    Fixed install issues

--- script/setup.sh
+++ script/setup.sh
@@ -4,27 +4,14 @@
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # Zsh plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 
 # oh-my-posh
-arch=""
-case $(uname -m) in
-    i386)    arch="386" ;;
-    i686)    arch="386" ;;
-    x86_64)  arch="amd64" ;;
-    aarch64) arch="arm64" ;;
-    armv71)  arch="arm" ;;
-    arm)     dpkg --print-architecture | grep -q "arm64" && arch="arm64" || arch="arm" ;;
-esac
-wget "https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-$arch" -O /usr/local/bin/oh-my-posh
-chmod a+rx /usr/local/bin/oh-my-posh
+curl -s https://ohmyposh.dev/install.sh | bash -s
 
 # vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Install tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-# Install Packer
-git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
