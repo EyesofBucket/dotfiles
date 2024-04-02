f10194076b85ede1882660b0ccbbd75f2bb60ad8
EyesofBucket <48115604+EyesofBucket@users.noreply.github.com>
2024-04-01 22:58:05 -0500

    Added cht.sh autocompletion

--- dotfiles/.zshrc.d/40-env.sh
+++ dotfiles/.zshrc.d/40-env.sh
@@ -1,4 +1,8 @@
-if which podman >/dev/null 2>&1
+if which nvim >/dev/null 2>&1
 then
     export EDITOR="$(which nvim)"
+else
+    export EDITOR="$(which vim)"
 fi
+
+fpath=(~/.zshrc.d/fpath $fpath)
