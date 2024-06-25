f5a902b3fb20e273b3cc86ab331ed6c34bd6cd89
EyesofBucket <48115604+EyesofBucket@users.noreply.github.com>
2024-06-25 09:25:42 -0500

    Oil show hidden

--- dotfiles/.config/nvim/lua/plugins/oil.lua
+++ dotfiles/.config/nvim/lua/plugins/oil.lua
@@ -1,5 +1,9 @@
 return {
     'stevearc/oil.nvim',
-    opts = {},
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+        view_options = {
+            show_hidden = true,
+        },
+    },
 }
