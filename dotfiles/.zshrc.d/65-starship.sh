077f948b95af9eb8836e327ffd8b0ea755379de6
eyesofbucket <bucket@example.com>
2025-06-30 02:30:29 -0500

    Replaced oh-my-posh with starship

--- dotfiles/.zshrc.d/65-starship.sh
+++ dotfiles/.zshrc.d/65-starship.sh
@@ -1,1 +1,6 @@
-#eval "$(starship init zsh)"
+function bvktdepth() {
+    export BVKT_DEPTH="$(dirs -p | tail -n +2 | wc -l | sed 's/ //g')"
+}
+typeset -a precmd_functions
+precmd_functions+=(bvktdepth)
+eval "$(starship init zsh)"
