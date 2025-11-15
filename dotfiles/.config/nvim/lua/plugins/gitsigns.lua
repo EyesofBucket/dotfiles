169d867ecd66138b5e3692f2458350adf0e8fd67
Eyes of Bucket <bucket@example.com>
2025-11-15 03:48:52 -0600

    Added gitsigns keybindings

--- dotfiles/.config/nvim/lua/plugins/gitsigns.lua
+++ dotfiles/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,4 +1,16 @@
 return {
     "lewis6991/gitsigns.nvim",
     event = "BufReadPre",
+    keys = {
+        {
+            "<leader>gb",
+            "<CMD>Gitsigns blame<CR>",
+            desc = "Gitsigns: Show Blame",
+        },
+        {
+            "<leader>gl",
+            "<CMD>Gitsigns blame_line<CR>",
+            desc = "Gitsigns: Show Blame",
+        },
+    },
 }
