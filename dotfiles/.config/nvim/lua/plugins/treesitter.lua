0c0b69a2f55c1633dbb11117ca292953dbed5c28
Eyes of Bucket <bucket@example.com>
2026-04-27 23:25:11 -0500

    Fixed treesitter

--- dotfiles/.config/nvim/lua/plugins/treesitter.lua
+++ dotfiles/.config/nvim/lua/plugins/treesitter.lua
@@ -1,41 +1,45 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    event = { 'BufRead' },
-    branch = 'main',
-    build = ":TSUpdate",
-    opts = {
-        ensure_installed = {
-            "c",
-            "bash",
-            "dockerfile",
-            "git_config",
-            "git_rebase",
-            "gitcommit",
-            "gitignore",
-            "go",
-            "gotmpl",
-            "helm",
-            "html",
-            "javascript",
-            "json",
-            "lua",
-            "markdown",
-            "markdown_inline",
-            "nix",
-            "passwd",
-            "python",
-            "query",
-            "ssh_config",
-            "toml",
-            "vim",
-            "vimdoc",
-            "yaml",
-        },
-        sync_install = false,
-        additional_vim_regex_highlighting = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-    }
-  },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        lazy = false,
+        branch = 'main',
+        build = ":TSUpdate",
+        init = function()
+            vim.api.nvim_create_autocmd('FileType', { 
+                callback = function() 
+                    -- Enable treesitter highlighting and disable regex syntax
+                    pcall(vim.treesitter.start) 
+                    -- Enable treesitter-based indentation
+                    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()" 
+                end, 
+            })
+            require('nvim-treesitter').install {
+                "c",
+                "bash",
+                "dockerfile",
+                "git_config",
+                "git_rebase",
+                "gitcommit",
+                "gitignore",
+                "go",
+                "gotmpl",
+                "helm",
+                "html",
+                "javascript",
+                "json",
+                "lua",
+                "markdown",
+                "markdown_inline",
+                "nix",
+                "passwd",
+                "python",
+                "query",
+                "ssh_config",
+                "toml",
+                "vim",
+                "vimdoc",
+                "yaml",
+            }
+        end,
+    },
 }
